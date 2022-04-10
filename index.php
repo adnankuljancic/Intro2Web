39b5af5509773430d28b5d545325ee8f14c41257
Adnan Kuljančić <adnan.kuljancic@outlook.com>
2022-04-10 22:32:54 +0200

    DAO part 1

--- index.php
+++ index.php
@@ -2,7 +2,20 @@
 require 'vendor/autoload.php'; //run autoloader
 
 Flight::route('/', function(){ //define route and define function to handle request
-  echo 'Hello Adi!';
+$servername = "Intro2Web";
+$username = "root";
+$password = "";
+$schema = "todos";
+
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$schema", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
 });
 
 Flight::start();        //start FlightPHP
