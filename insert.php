abcdc594eeca59182eb00ecdbe218d07e6687736
Adnan Kuljančić <adnan.kuljancic@outlook.com>
2022-04-15 11:27:29 +0200

    DAO layer part 2

--- insert.php
+++ insert.php
@@ -1,7 +1,4 @@
 <?php
-require 'vendor/autoload.php'; //run autoloader
-
-Flight::route('/', function(){ //define route and define function to handle request
 $servername = "localhost";
 $username = "root";
 $password = "password";
@@ -13,14 +10,12 @@ try {
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
-  $stmt = $conn->prepare("SELECT * FROM todos");
-  $stmt -> execute();
-  $result = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+  $stmt = $conn->prepare("INSERT INTO todos(description, created) VALUES ('AdiTest','2022.04.14 22:29:00')");
+  $result = $stmt -> execute();
+  //$result = $stmt -> fetchAll(PDO::FETCH_ASSOC);
   print_r($result);
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
-});
 
-Flight::start();        //start FlightPHP
  ?>
