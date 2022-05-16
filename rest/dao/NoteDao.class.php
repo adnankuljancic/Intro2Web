948de9eb8d12492707cbd3ef84461b4259bcec09
Adnan Kuljančić <adnan.kuljancic@outlook.com>
2022-05-16 12:46:09 +0200

    BL layer

--- rest/dao/NoteDao.class.php
+++ rest/dao/NoteDao.class.php
@@ -1,11 +1,10 @@
 <?php
 require_once __DIR__.'/BaseDao.class.php'
-class TodoDao extends BaseDao{
+class NoteDao extends BaseDao{
   //constructor
   public function __construct() {
-    parent::__construct("todos");
+    parent::__construct("notes");
   }
-
   
 
 
---
+++ rest/services/NoteService.class.php
@@ -0,0 +1,13 @@
+<?php
+require_once __DIR__.'/BaseService.class.php';
+require_once __DIR__.'/../dao/NoteDao.class.php';
+class NoteService extends BaseService{
+  private $dao;
+  public function __construct() {
+     parent::__construct(new NoteDao());
+  }
+  
+
+}
+
+ ?>
